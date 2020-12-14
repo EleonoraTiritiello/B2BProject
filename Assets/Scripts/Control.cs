768fbd60c406355c54e7921e48eba584c8a47b7f
Nicolas <betteganicolas@hotmail.com>
2020-12-14 09:47:28 +0100

    3x3 Minigame

---
+++ Assets/Scripts/Control.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    private void FixedUpdate()
+    {
+        cacca();
+        if (cacca() == true)
+        {
+            Debug.Log("Hai vinto coglionazzo adesso vai a giocare a cyberpunk");
+            transform.GetChild(8).gameObject.SetActive(true);
+            for (int i = 0; i < gameObject.transform.childCount; i++)
+                gameObject.transform.GetChild(i).GetComponent<Movement>().isSelectable = false;
+
+
+        }
+    }
+    private bool cacca()
+    {
+        for (int i = 0; i < gameObject.transform.childCount; i++)
+        {
+            if (gameObject.transform.GetChild(i).GetComponent<Movement>().rightPosition == false)
+                return false;
+        }
+        return true;
+    }
+}
---
+++ Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    public float NewPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+            transform.position = new Vector3(transform.position.x + NewPosition, 1, -5.141f);
+        if (Input.GetKeyDown(KeyCode.O))
+            transform.position = new Vector3(transform.position.x - NewPosition, 1, -5.141f);
+    }
+}
