b47f71bf8f936728a9ca5c8cd8d6fb1870f92917
Francesco Guida <fraguieh@gmail.com>
2020-12-19 00:51:09 +0100

    Main Menu Arrows Movement
    - Arrows added to move into main menu
    - Swipe movement removed to the main menu

---
+++ Assets/Scripts/UI_Scripts/Arrows.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Arrows : MonoBehaviour
+{
+    [SerializeField] private Button previous;
+    [SerializeField] private Button next;
+
+    private int currentBox;
+
+    private void Awake()
+    {
+        SelectBox(0);
+    }
+
+
+    private void SelectBox(int _index)
+    {
+        previous.interactable = (_index != 0);
+        next.interactable = (_index != transform.childCount - 1);
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(i == _index);
+        }
+    }
+
+    public void changeBox(int _change)
+    {
+        currentBox += _change;
+        SelectBox(currentBox);
+    }
+}
